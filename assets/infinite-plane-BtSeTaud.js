a2ebc71dbc9ee5701bdee48d498f01225814227d
atharva038 <atharva038@users.noreply.github.com>
2026-05-01 08:14:33 +0000

    deploy: 6f51ecaeff643de647a185d06ec032e93bd38c97

--- assets/infinite-plane-BtSeTaud.js
+++ assets/infinite-plane-BtSeTaud.js
@@ -1,4 +1,4 @@
-import{r as i,j as m}from"./index-D33tCLlV.js";const N=`#version 300 es
+import{r as i,j as m}from"./index-DHsG2mvb.js";const N=`#version 300 es
 precision mediump float;
 
 uniform float u_time;
