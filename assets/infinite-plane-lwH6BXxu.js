fe82863dff0f92d3a8687994db1534b7dec42907
atharva038 <atharva038@users.noreply.github.com>
2026-05-02 14:41:06 +0000

    deploy: 826210c5edbae8f92367c844d7c2f6b1a4479f39

--- assets/infinite-plane-lwH6BXxu.js
+++ assets/infinite-plane-lwH6BXxu.js
@@ -1,4 +1,4 @@
-import{r as i,j as m}from"./index-BO3p5MRe.js";const N=`#version 300 es
+import{r as i,j as m}from"./index-BHRPhtUF.js";const N=`#version 300 es
 precision mediump float;
 
 uniform float u_time;
