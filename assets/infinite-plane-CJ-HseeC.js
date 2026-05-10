4420905ef204f0979249a01762afef08dc6116f5
atharva038 <atharva038@users.noreply.github.com>
2026-05-10 10:55:39 +0000

    deploy: 10223883a9a2dc334d2cbb2d92357aa90716fbbd

--- assets/infinite-plane-CJ-HseeC.js
+++ assets/infinite-plane-CJ-HseeC.js
@@ -1,4 +1,4 @@
-import{r as i,j as m}from"./index-3El9bxnH.js";const N=`#version 300 es
+import{r as i,j as m}from"./index-C3lR5Yoy.js";const N=`#version 300 es
 precision mediump float;
 
 uniform float u_time;
