a006da2025ae97dfe9780ff527c9c63a811f3da8
atharva038 <atharvasjoshi2005@gmail.com>
2026-07-05 18:55:45 +0530

    Updates

--- assets/infinite-plane-CXwuvC7e.js
+++ assets/infinite-plane-CXwuvC7e.js
@@ -1,4 +1,4 @@
-import{r as i,j as m}from"./index-72_g17Sa.js";const N=`#version 300 es
+import{r as i,j as m}from"./index-C0n-gh7x.js";const N=`#version 300 es
 precision mediump float;
 
 uniform float u_time;
